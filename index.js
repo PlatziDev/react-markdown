c97f98a7c8635f5fa0527b27ec4046ff3fbb020e
Sergio Daniel Xalambrí <sergiodxa@gmail.com>
2017-01-13 01:58:27 -0500

    [add] write component

---
+++ index.js
@@ -0,0 +1,33 @@
+const React = require('react');
+const createParser = require('@platzi/markdown');
+
+
+function Markdown(props) {
+  const tagProps = Object
+    .keys(props)
+    .filter(key => (
+      key === 'tagName' || key === 'content' || key === 'parser'
+    ))
+    .reduce((map, key) => map[key] = props[key], {});
+
+  const parser = createParser(props.parser);
+
+  return React.createElement(props.tagName, Object.assign(tagProps, {
+    dangerouslySetInnerHTML: {
+      __html: parser(props.content),
+    }
+  }));
+}
+
+Markdown.propTypes = {
+  tagName: React.PropTypes.string,
+  content: React.PropTypes.string,
+};
+Markdown.defaultProps = {
+  tagName: 'div',
+  content: '',
+  parser: {},
+};
+
+
+module.exports = Markdown;
