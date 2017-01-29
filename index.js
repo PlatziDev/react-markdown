fa75e6dc7a8f32851343d0be1b990b35186d1da2
Sergio Daniel Xalambrí <sergiodxa@gmail.com>
2017-01-28 20:32:45 -0500

    [update] avoid using arrow functions, document code and add fix prop types

--- index.js
+++ index.js
@@ -2,15 +2,40 @@ const React = require('react');
 const createParser = require('@platzi/markdown');
 
 
+/**
+ * Filter custom props
+ * @param  {string}  key The props name
+ * @return {boolean}     If it's a custom prop or not
+ */
+function filter(key) {
+  return key !== 'tagName' && key !== 'content' && key !== 'parser';
+}
+
+/**
+ * Merge props into a new map
+ * @param  {Object} props The original prop map
+ * @param  {Object} map   The new prop map to fill
+ * @param  {string} key   Each key name that have passed the filter
+ * @return {Object}       The new prop map with the setted prop
+ */
+function reducer(props, map, key) {
+  return Object.assign({}, map, {
+    [key]: props[key]
+  });
+}
+
+
+/**
+ * Render Platzi Flavored Markdown inside a React application.
+ * @param {string} [props.tagName='div'] The HTML tag used as wrapper
+ * @param {string} props.content         The Markdown content to parse and render
+ * @param {Object} [props.parser={}]     The parser options
+ */
 function Markdown(props) {
   const tagProps = Object
     .keys(props)
-    .filter(key => (
-      key !== 'tagName' && key !== 'content' && key !== 'parser'
-    ))
-    .reduce((map, key) => (
-      Object.assign({}, map, { [key]: props[key] })
-    ), {});
+    .filter(filter)
+    .reduce(reducer.bind(null, props), {});
 
   const parser = createParser(props.parser);
 
@@ -21,13 +46,15 @@ function Markdown(props) {
   }));
 }
 
+
 Markdown.propTypes = {
   tagName: React.PropTypes.string,
-  content: React.PropTypes.string,
+  content: React.PropTypes.string.isRequired,
+  parser: React.PropTypes.object,
 };
+
 Markdown.defaultProps = {
   tagName: 'div',
-  content: '',
   parser: {},
 };
 
